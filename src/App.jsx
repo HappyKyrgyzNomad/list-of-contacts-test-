3d057936831a2c75cee0e4a023cbf8def482660b
HappyKyrgyzNomad <kanatbeks.son@gmail.com>
2023-03-24 12:45:40 +0600

    Fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import "./App.scss";
 
-import SearchBar from "./components/SearchBar/SearchBar";
-import Footer from "./components/Footer/Footer";
 import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div>
       <div className="App">
-        <SearchBar />
         <Card />
       </div>
-      <Footer />
     </div>
   );
 }
